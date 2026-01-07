56058320c7fdee0540ff2bd45fa989e96c93e273
adxreel3-demo <adxreel3@gmail.com>
2026-01-08 01:59:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log("OPENAI KEY LOADED:", !!process.env.OPENAI_API_KEY);
   console.log(`Backend running on port ${PORT}`);
 });
+
