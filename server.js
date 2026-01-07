7a738914db2afd69d15293e7cb67e4bb5432ec84
adxreel3-demo <adxreel3@gmail.com>
2026-01-08 02:10:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+if (!PORT) {
+  throw new Error("PORT is not defined");
+}
+app.listen(PORT, "0.0.0.0", () => {
   console.log("OPENAI KEY LOADED:", !!process.env.OPENAI_API_KEY);
   console.log(`Backend running on port ${PORT}`);
 });
+
 
 
